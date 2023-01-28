2ea4df2611542be47183e51b905f5e9fb82728d2
Anzhelika Shakurova <anzelikasht@gmail.com>
2023-01-29 01:01:16 +0300

    versions updated, version extracted to gradle.properties file

--- src/main/java/com/example/demo/model/Item.java
+++ src/main/java/com/example/demo/model/Item.java
@@ -1,11 +1,11 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 import java.util.UUID;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
